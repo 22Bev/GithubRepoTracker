800bd4bb1849d0bdf3d2012fae93aeb2638bf100
22Bev <95314580+22Bev@users.noreply.github.com>
2025-08-30 01:08:02 +0300

    add usage instructions for CLI arguments

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("")
+		fmt.Println("Usage: go run main.go <Owner>/<Repo>")
+	}
+	command := os.Args[1]
+	repo := os.Args[2]
+	switch command {
+	case "Repo":
+		info, err := GetRepoInfo(repo)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		PrintRepoInfo(info)
+
+	default:
+		fmt.Println("Unknown command:", command)
 	}
 }
